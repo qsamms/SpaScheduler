5dff4f0f78399ff29bcd945d5aaa7acb3cb5e9ca
Quinn Samms <qsamms11@gmail.com>
2022-12-04 16:54:38 -0800

    added javadoc and UML pdf

--- SpaScheduler/src/Spa.java
+++ SpaScheduler/src/Spa.java
@@ -1,16 +1,34 @@
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/** lab1 drawing with basic java 2D classes
+ * 
+ * @author quinnsamms
+ *
+ */
 public class Spa{
+	/**
+	 * general information about spa
+	 *
+	 */
 	private String name;
 	private String location;
 	
+	/**
+	 * 
+	 * ArrayLists to hold instances of classes
+	 */
 	private ArrayList<Appointment> schedule;
 	private ArrayList<Treatment> treatments;
 	private ArrayList<Therapist> therapists;
 	private ArrayList<Customer> customers;
 	
+	/**
+	 * 
+	 * @param n
+	 * @param l
+	 * constuctor for spa, takes a name and location for spa 
+	 */
 	public Spa(String n, String l) {
 		name = n;
 		location = l;
@@ -29,6 +47,9 @@ public class Spa{
 		treatments.add(Waxing);
 	}
 	
+	/**
+	 * prints info about the spa onto the terminal
+	 */
 	public void printSchedule() {
 		HashMap<Integer,String> trav = new HashMap<Integer,String>();
 		
@@ -42,9 +63,9 @@ public class Spa{
 		
 		System.out.println("");
 		for(int i = 0;i<7;i++) {
+			System.out.println(trav.get(i) + ": ");
+			System.out.println("");
 			for(int j = 0;j<schedule.size();j++) {
-				System.out.println(trav.get(i) + ": ");
-				System.out.println("");
 				if(schedule.get(j).getDay().equals(trav.get(i))){
 					Appointment a = schedule.get(j);
 					System.out.println("Appointment: ");
@@ -58,39 +79,76 @@ public class Spa{
 		}
 	}
 	
+	
+	/** 
+	 * returns the name of the spa 
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * returns location of spa
+	 * @return
+	 */
 	public String getLocation() {
 		return location;
 	}
 	
+	/**
+	 * adds an apopintment to appointment arraylist
+	 * @param a
+	 */
 	public void setAppointment(Appointment a) {
 		schedule.add(a);
 	}
 	
+	/**
+	 * adds a therapist to therapist arrayList
+	 * @param t
+	 */
 	public void addTherapist(Therapist t) {
 		therapists.add(t);
 	}
 	
+	/**
+	 * removes therapist from therapist arraylist
+	 * @param t
+	 */
 	public void removeTherapist(Therapist t) {
 		therapists.remove(t);
 	}
 	
-	
+	/**
+	 * adds customer to customer arraylist
+	 * @param c
+	 */
 	public void addCustomer(Customer c) {
 		customers.add(c);
 	}
 	
+	/**
+	 * removes customer from customer arraylist
+	 * @param c
+	 */
 	public void removeCustomer(Customer c) {
 		customers.remove(c);
 	}
 	
+	/**
+	 * returns all treatments as arraylist
+	 * @return
+	 */
 	public ArrayList<Treatment> getTreatments() {
 		return treatments;
 	}
 	
+	/**
+	 * returns specific treatment instance from a given name, returns null if not found
+	 * @param treatment
+	 * @return
+	 */
 	public Treatment getTreatment(String treatment) {
 		
 		for(int i = 0;i<treatments.size();i++) {
@@ -102,10 +160,18 @@ public class Spa{
 		return null;
 	}
 	
+	/**
+	 * returns all therapists as arraylist
+	 * @return
+	 */
 	public ArrayList<Therapist> getTherapists() {
 		return therapists;
 	}
 	
+	/**
+	 * retuns all therapists as a string array
+	 * @return
+	 */
 	public String[] getTherapistsAsString() {
 		String[] ther = new String[therapists.size()];
 		
@@ -116,10 +182,18 @@ public class Spa{
 		return ther;
 	}
 	
+	/**
+	 * returns all customers as arraylist
+	 * @return
+	 */
 	public ArrayList<Customer> getCustomers() {
 		return customers;
 	}
 	
+	/**
+	 * returns all customers as a string
+	 * @return
+	 */
 	public String[] getCustomersAsString() {
 		String[] custs = new String[customers.size()];
 		
@@ -130,6 +204,11 @@ public class Spa{
 		return custs;
 	}
 	
+	/**
+	 * returns specific therapist instance from a given name, returns null otherwise
+	 * @param name
+	 * @return
+	 */
 	Therapist getTherapist(String name) {
 		
 		for(int i = 0;i<therapists.size();i++) {
@@ -141,6 +220,11 @@ public class Spa{
 		return null;
 	}
 	
+	/**
+	 * returns specific customer instance from a given name, returns null otherwise
+	 * @param name
+	 * @return
+	 */
 	Customer getCustomer(String name) {
 		
 		for(int i = 0;i<customers.size();i++) {
